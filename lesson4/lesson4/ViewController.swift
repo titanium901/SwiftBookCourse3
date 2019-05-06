9d7ead1dee8723f2402f9bbde7ef6583f22a0475
Iurii Popov <30910230+titanium901@users.noreply.github.com>
2019-05-06 21:43:08 +0300

    Add files via upload

---
+++ lesson4/lesson4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson4
+//
+//  Created by Yury Popov on 06/05/2019.
+//  Copyright © 2019 Yury Popov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
